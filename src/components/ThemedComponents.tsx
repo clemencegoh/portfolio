7f9b90ee36ea27fe4be5b7dcca01348f10a9fab8
Clemence Goh <clemencegoh@gmail.com>
2022-02-11 10:59:08 +0800

    add bottom navigation explorer for project details page

--- src/components/ThemedComponents.tsx
+++ src/components/ThemedComponents.tsx
@@ -1,5 +1,11 @@
 import { IconButton, styled } from "@mui/material";
+import { Link } from "react-router-dom";
 
 export const ThemedIconButton = styled(IconButton)`
+  color: var(--text-main);
+`;
+
+export const ThemedLink = styled(Link)`
+  text-decoration: none;
   color: var(--text-main);
 `;
--- src/pages/projects/details/ProjectDetailsPage.tsx
+++ src/pages/projects/details/ProjectDetailsPage.tsx
@@ -1,17 +1,34 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { projectsData } from "data/projects";
-import { Button, Card, Container, Fade, Grid, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  Container,
+  Fade,
+  Grid,
+  Typography,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import { css } from "@emotion/css";
 import IPhoneCase from "./IPhoneCase";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import GridViewIcon from "@mui/icons-material/GridView";
+import { ThemedIconButton, ThemedLink } from "components/ThemedComponents";
 
 type Props = {};
 
 export default function ProjectDetailsPage({}: Props) {
   const { name } = useParams();
-  const data = projectsData.find((item) => item.innerName === name);
+  const dataIndex = projectsData.findIndex((item) => item.innerName === name);
+  const data = dataIndex !== -1 ? projectsData[dataIndex] : null;
+
+  if (!data) {
+    return <></>;
+  }
 
   return (
     <Container>
@@ -90,7 +107,7 @@ export default function ProjectDetailsPage({}: Props) {
           flex-flow: row;
           justify-content: space-around;
           color: var(--text-main);
-          margin-top: 2rem;
+          margin: 2rem 0;
 
           @media screen and (max-width: 600px) {
             flex-flow: column;
@@ -133,28 +150,160 @@ export default function ProjectDetailsPage({}: Props) {
 
         <article
           className={css`
-            display: flex;
-            flex-direction: column;
             flex: 1;
+            padding-top: 3rem;
           `}
         >
-          <Typography>{data?.description}</Typography>
-          <Typography>Completion Date</Typography>
-          <Typography>{data?.completionDate ?? "Work in progress"}</Typography>
-          <Typography>Tech Stack</Typography>
-          <Typography>{data?.tags.join(", ")}</Typography>
-          <Button endIcon={<ChevronRightIcon />}>Check it out</Button>
+          <Typography variant="body1" paragraph>
+            {data?.description}
+          </Typography>
+          <Typography variant="h6" sx={{ textTransform: "uppercase" }}>
+            Completion Date
+          </Typography>
+          <Typography paragraph>
+            {data?.completionDate ?? "Work in progress"}
+          </Typography>
+          <Typography variant="h6" sx={{ textTransform: "uppercase" }}>
+            Tech Stack
+          </Typography>
+          <Typography paragraph>{data?.tags.join(", ")}</Typography>
+          <Button
+            sx={{ marginTop: "2rem" }}
+            size="large"
+            variant="contained"
+            endIcon={<ChevronRightIcon />}
+          >
+            Check it out
+          </Button>
         </article>
       </section>
       <section id="other-images">
-        <Grid>
+        <Grid container spacing={2}>
           {data?.otherSrc.map((item, index) => (
-            <img
-              alt={`sample ${index}`}
-              src={`${process.env.PUBLIC_URL}/images/${item}`}
-            />
+            <Grid item xs={12} md={6}>
+              <img
+                className={css`
+                  width: 100%;
+                `}
+                alt={`sample ${index}`}
+                src={`${process.env.PUBLIC_URL}/images/${item}`}
+              />
+            </Grid>
           ))}
         </Grid>
+      </section>
+      <section
+        id="project-navigation"
+        className={css`
+          display: flex;
+          justify-content: space-between;
+        `}
+      >
+        <div
+          id="previous-project"
+          className={css`
+            flex: 1;
+            @media screen and (max-width: 600px) {
+              display: none;
+            }
+          `}
+        >
+          {dataIndex - 1 > -1 ? (
+            <Button variant="text" sx={{ textTransform: "none" }}>
+              <ThemedLink
+                to={`/portfolio/projects/${
+                  projectsData[dataIndex - 1].innerName
+                }`}
+              >
+                <div
+                  className={css`
+                    display: flex;
+                    align-items: center;
+                    text-align: left;
+                  `}
+                >
+                  <ChevronLeftIcon sx={{ marginRight: "1rem" }} />
+                  <div>
+                    <Typography
+                      sx={{ marginBottom: "-0.5rem" }}
+                      gutterBottom
+                      variant="overline"
+                      component="div"
+                    >
+                      PREVIOUS PROJECT
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      {projectsData[dataIndex - 1]?.name}
+                    </Typography>
+                  </div>
+                </div>
+              </ThemedLink>
+            </Button>
+          ) : (
+            ""
+          )}
+        </div>
+        <div
+          id="project-main"
+          className={css`
+            flex: 1;
+            align-self: center;
+            text-align: center;
+            @media screen and (max-width: 600px) {
+              display: none;
+            }
+          `}
+        >
+          <ThemedIconButton>
+            <ThemedLink to={`/portfolio/projects`}>
+              <GridViewIcon />
+            </ThemedLink>
+          </ThemedIconButton>
+        </div>
+        <div
+          id="next-project"
+          className={css`
+            flex: 1;
+            display: flex;
+            justify-content: right;
+          `}
+        >
+          {dataIndex + 1 < projectsData.length ? (
+            <Button variant="text" sx={{ textTransform: "none" }}>
+              <ThemedLink
+                to={`/portfolio/projects/${
+                  projectsData[dataIndex + 1].innerName
+                }`}
+              >
+                <div
+                  className={css`
+                    display: flex;
+                    align-items: center;
+                    text-align: right;
+                  `}
+                >
+                  <div>
+                    <Typography
+                      sx={{ marginBottom: "-0.5rem" }}
+                      gutterBottom
+                      variant="overline"
+                      component="div"
+                    >
+                      NEXT PROJECT
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      {projectsData[dataIndex + 1]?.name}
+                    </Typography>
+                  </div>
+
+                  <ChevronRightIcon sx={{ marginLeft: "1rem" }} />
+                </div>
+              </ThemedLink>
+            </Button>
+          ) : (
+            ""
+          )}
+        </div>
       </section>
     </Container>
   );
